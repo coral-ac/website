d2a07c55d6cbc342c72047d742ef170be79cf029
Tom <tom@mono.wales>
2024-04-24 12:44:45 +0100

    Draft copy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,3 @@
-🪸
+<div class={'flex w-full h-screen items-center justify-center'}>
+    <h1>🪸 coral</h1>
+</div>
---
+++ src/routes/preview/+page.svelte
@@ -0,0 +1,53 @@
+<div class={'m-10'}>
+    <h1>🪸 coral</h1>
+    <div class={'text-5xl py-8 border-b border-[#ff0048] border-dashed text-balance leading-[4rem]'}>
+        Tools and methods
+        for co-creating research – made for humans
+    </div>
+    <div class="my-4">
+        People
+    </div>
+    <div class={'text-base flex flex-wrap gap-2 justify-center font-bold lg:w-2/3 lg:mx-auto'}>
+        <div>Ruth Patrick <small class={'font-mono lowercase font-normal'}>York</small></div>
+        <div>Maddy Power <small class={'font-mono lowercase font-normal'}>York</small></div>
+        <div>Jim Kaufman <small class={'font-mono lowercase font-normal'}>Salford</small></div>
+        <div>Jean Mcewan <small class={'font-mono lowercase font-normal'}>Artist</small></div>
+        <div>Tom Flannery <small class={'font-mono lowercase font-normal'}>Fork</small></div>
+        <div>Parents + carers <small class={'font-mono lowercase font-normal'}>Covid + Changing realities</small></div>
+        <div>You? <small class={'font-mono lowercase font-normal'}>Say hello</small></div>
+    </div>
+
+    <div class="grid lg:grid-cols-2 gap-8 mt-10 justify-center items-center">
+        <div class="border-[#ff0048] p-5">
+            ↯ Training
+            <div class="text-sm font-mono my-6 leading-relaxed">
+                We've been working as a group for four years with parents living in poverty,
+                developing a tried and tested participatory method for researching remotely as a group.
+            </div>
+            <button class="text-center border border-[#ff0048] rounded-full rounded text-sm font-bold tracking-wide font-mono p-2 px-6 bg-amber-100">
+                Get trained
+            </button>
+        </div>
+
+        <div class="border-[#ff0048] p-5">
+            ↯ Open source tools
+            <div class="text-sm font-mono my-6 leading-relaxed">
+                We're building ambitious, open and humane technology to allow researchers
+                to do their work, online with peers and in line with their values.
+            </div>
+            <button class="text-center border border-[#ff0048] rounded-full rounded text-sm font-bold tracking-wide font-mono p-2 px-6 bg-amber-100">
+                Get the tools
+            </button>
+        </div>
+    </div>
+</div>
+
+<div class="relative">
+    <div class="bg-[#ff0048] mix-blend-darken">
+        <img src="/coral.gif" class="mix-blend-screen" alt="Big coral ecosystem" />
+    </div>
+    <div class="absolute text-balance leading-relaxed text-xs w-3/4 md:w-1/2 rounded-full lg:text-sm font-mono bg-[#dcfef5] p-4 pb-5 px-12 top-1/2 left-1/2 -translate-x-1/2">
+        <strong>🪸corals</strong> join together and build strong
+        structures to support flourishing ecosystems. 
+    </div>
+</div>
